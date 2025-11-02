9ba0e53f8dfd68849389af1bcedc7b704c92aff4
Lishwar333 <jayaklish@gmail.com>
2025-11-02 22:37:23 +0100

    Error panel for maintainence break ad Payment panel for payment

---
+++ src/myswing/gui/panels/MainPanel.java
@@ -0,0 +1,33 @@
+package myswing.gui.panels;
+
+import java.awt.CardLayout;
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+public class MainPanel extends JPanel {
+	
+	HomePanel homePanel;
+	OrderPanel orderPanel;
+	ErrorPanel errorPanel;
+	PaymentPanel paymentPanel;
+	
+	
+	public MainPanel(CardLayout cards) {
+		
+		homePanel = new HomePanel(this, cards);
+		orderPanel = new OrderPanel(this, cards);
+		errorPanel = new ErrorPanel();
+		paymentPanel = new PaymentPanel();
+		
+		this.setLayout(cards);
+		this.setBackground(Color.yellow);
+		this.add(homePanel, "Home");
+		this.add(orderPanel, "Orders");
+		this.add(errorPanel, "Error");
+		this.add(paymentPanel);
+		
+		
+	}
+
+}
---
+++ src/myswing/gui/panels/OrderListPanel.java
@@ -0,0 +1,12 @@
+package myswing.gui.panels;
+
+import javax.swing.JPanel;
+
+public class OrderListPanel extends JPanel{
+	
+	public OrderListPanel() {
+		
+		
+	}
+
+}
